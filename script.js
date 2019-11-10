7953ff08afbc080530179183f3eee5ae483758a0
sapla07 <s.sapla132@gmail.com>
2019-11-11 01:05:11 +0530

    updated code

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-const ul = document.getElementById('movies');
+const div = document.getElementById('movies');
+div.setAttribute("class", "row");
+
+
 const url = 'https://www.omdbapi.com/?apikey=d14b5a69&s=batman';
 
 function createNode(element){
     return document.createElement(element);
 }
+
+function sortByTitle(movies) {
+  movies.sort(function(a, b){
+	var s1 = a.Title.toLowerCase();
+	var s2 = b.Title.toLowerCase();
+	if (s1 < s2) {return -1;}
+	if (s1 > s2) {return 1;}
+	return 0;
+	});
+	
+	return movies;
+}
+
+function sortByYear(movies) {
+    movies.sort(function(a, b){
+	var s1 = a.Year.toLowerCase();
+	var s2 = b.Year.toLowerCase();
+	if (s1 < s2) {return -1;}
+	if (s1 > s2) {return 1;}
+	return 0;
+	});
+	
+	return movies;
+}
+
 
 function append(parent, el){
     return parent.appendChild(el);
@@ -15,14 +43,111 @@ fetch(url)
       console.log(JSON.stringify(data))
       let movies = data.Search;
        return movies.map(function(movie){
-          let li = createNode('li'),
+          let div1 = createNode('div'),
               img = createNode('img'),
-              span = createNode('span');
-           img.src = movie.Poster;
-           span.innerHTML = `${movie.Title} ${movie.Year}`;
-           append(li, span);
-           append(li, img\\);
-           append(ul, li);       
+               p = createNode('p');
+             // h4 = createNode('h4');
+                     
+           img.src = movie.Poster;           
+           p.innerHTML = `${movie.Title}  ${movie.Year}`;
+           
+          
+           div1.setAttribute("class", "column")
+           img.setAttribute("style", "width: 300px; height: 300px" );
+        //    append(div1, h3);
+           append(div1, img);
+           append(div1, p)
+           append(div, div1);       
       })
  } )
  .catch((error)=>{ console.log(error)});
+
+
+
+
+// completely chaneged searchMovies Function
+
+ function searchMovies(){
+ 
+		var input = document.getElementById('search');
+		var filter = input.value.toUpperCase();
+		fetch(url)
+		.then( function(response){ return response.json() } )
+			.then( function(data){
+				//console.log(JSON.stringify(data))
+				let movies = data.Search;
+				console.log(movies);
+				div.innerHTML = ""; 
+				
+				return movies.map(function(movie){	
+					var title = movie.Title
+					if(title.toUpperCase().indexOf(filter) > -1){
+						let div1 = createNode('div'),
+						img = createNode('img'),
+						p = createNode('p');
+
+						img.src = movie.Poster;           
+						p.innerHTML = `${movie.Title}  ${movie.Year}`;
+
+
+						div1.setAttribute("class", "column")
+						img.setAttribute("style", "width: 300px; height: 300px" );
+					//    append(div1, h3);
+						append(div1, img);
+						append(div1, p)
+						append(div, div1);       
+					}
+					
+				})	
+			})
+		.catch((error)=>{ console.log(error)});
+	
+ 		
+ }
+ 
+ 
+ 
+ 
+ // Added New Function for Filtering
+ 
+ 
+ function filter(choice) {
+  //if (choice=="default") return; // please select - do nothing
+	console.log("selected field is", choice)
+	
+		fetch(url)
+		.then( function(response){ return response.json() } )
+			.then( function(data){
+				//console.log(JSON.stringify(data))
+				let movies = data.Search;
+				console.log(movies);
+				div.innerHTML = ""; 
+				
+				if (choice.toLowerCase() == "title" ){
+						movies = sortByTitle(movies);
+				} else if (choice.toLowerCase() == "year" ){ 
+						movies = sortByYear(movies);
+				}
+				console.log("sorted",movies)
+				return movies.map(function(movie){
+					let div1 = createNode('div'),
+					img = createNode('img'),
+					p = createNode('p');
+					// h4 = createNode('h4');
+
+					img.src = movie.Poster;           
+					p.innerHTML = `${movie.Title}  ${movie.Year}`;
+
+
+					div1.setAttribute("class", "column")
+					img.setAttribute("style", "width: 300px; height: 300px" );
+					//    append(div1, h3);
+					append(div1, img);
+					append(div1, p)
+					append(div, div1);       
+				})	
+			})
+		.catch((error)=>{ console.log(error)});
+	
+
+}
